b0578d80c64b47db11213e4ec1299e947bcb1a62
Bujar Roka <rokabujar@gmail.com>
2024-12-06 20:05:08 +0100

    created the menu design

--- schoolmanager/src/app/(dashboard)/layout.tsx
+++ schoolmanager/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,29 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <div className="h-screen flex">
-        {/* LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">left</div>
-        {/* RIGHT */}
-        <div className="w-[86%] md:w-[96%] lg:w-[84%] xl:w-[86%] bg-blue-200">right</div>
-        </div> 
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block"> SchoolManager</span>
+        </Link>
+        <Menu />
+
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[96%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
   );
 }
