4e6196f342fb8d572b8f3be9f440e8ddab8e48f1
Bujar Roka <rokabujar@gmail.com>
2024-12-08 00:28:26 +0100

    fixed a small menu item bug

--- schoolmanager/src/components/Menu.tsx
+++ schoolmanager/src/components/Menu.tsx
@@ -119,12 +119,12 @@ const menuItems = [
 const Menu = () => {
     return (
         <div className='mt-4 text-sm'>
-            {menuItems.map(i => (
+            {menuItems.map((i) => (
                 <div className="flex flex-col gap-2" key={i.title}>
                     <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-                    {i.items.map(item =>
+                    {i.items.map((item) =>
                         <Link href={item.href} key={item.label} 
-                        className="flex items-center justify-center lg:justify-end gap-4 text-gray-500 py-2">
+                        className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
                             <Image src={item.icon} alt="" width={20} height={20} />
                             <span className="hidden lg:block ">{item.label}</span>
                         </Link>
