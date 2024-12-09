8e6dddcd9baa148fae1a48f28f180806a152260e
Bujar Roka <rokabujar@gmail.com>
2024-12-09 19:25:24 +0100

    Created announcement component design

--- schoolmanager/src/app/(dashboard)/layout.tsx
+++ schoolmanager/src/app/(dashboard)/layout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block"> SchoolManager</span>
+          <span className="hidden lg:block font-bold"> SchoolManager</span>
         </Link>
         <Menu />
       </div>
