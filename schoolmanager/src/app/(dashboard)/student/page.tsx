78aa7b1ddd63c30cba1d9c4d4fae84c141218f3d
Bujar Roka <rokabujar@gmail.com>
2024-12-10 18:33:34 +0100

    strated the Student dashboard design

--- schoolmanager/src/app/(dashboard)/student/page.tsx
+++ schoolmanager/src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,22 @@
+import Announcements from "@/components/Announcements";
+import EventCalendar from "@/components/EventCalendar";
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT */}
+      <div className="w-full xl:2/3">
+      <div className="h-full bg-white p4 rounded-md">
+        <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+      </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8" >
+        <EventCalendar />
+        <Announcements />
+      </div>
+
+    </div>
   );
 };
 
