f2d399e4bbf3579b1fab49cbd4677288fe059bed
Bujar Roka <rokabujar@gmail.com>
2024-12-30 19:55:58 +0100

    updated the event calendar to fetch data from database (admin page)

--- schoolmanager/src/components/EventCalendar.tsx
+++ schoolmanager/src/components/EventCalendar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { time } from "console";
-import Image from "next/image";
-import { title } from "process";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
@@ -11,53 +9,18 @@ type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-//TEMPORARY
-const events = [
-    {
-        id: 1,
-        title: "Lorem ipsum dolor",
-        time: "12:00 PM - 2:00 PM",
-        description: "Lorem ipsum dolor sit amet, consectetur adipuscing elit.",
-    },
-    {
-        id: 2,
-        title: "Lorem ipsum dolor",
-        time: "12:00 PM - 2:00 PM",
-        description: "Lorem ipsum dolor sit amet, consectetur adipuscing elit.",
-    },
-    {
-        id: 3,
-        title: "Lorem ipsum dolor",
-        time: "12:00 PM - 2:00 PM",
-        description: "Lorem ipsum dolor sit amet, consectetur adipuscing elit.",
-    },
-];
-
 const EventCalendar = () => {
     const [value, onChange] = useState<Value>(new Date());
 
-    return (
-        <div className="bg-white p-4 rounded-md">
-            <Calendar onChange={onChange} value={value} />
-            <div className="flex items-center justify-between"></div>
-            <h1 className="text-xl font-semibold my-4">Events</h1>
-            <Image src="/moreDark.png" alt="" width={20} height={20} />
-            <div className="flex flex-col gap-4">
-                {events.map((event) => (
-                    <div
-                        className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
-                        key={event.id}
-                    >
-                        <div className="flex items-center justify-between">
-                            <h1 className="semi-bold text-gray-600">{event.title}</h1>
-                            <span className="text-gray-300 text-xs">{event.time}</span>
-                        </div>
-                        <p className="mt-2 text-gray-400">{event.description}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        if (value instanceof Date) {
+            router.push(`?date=${value.toLocaleDateString("en-US")}`);
+        }
+    }, [value, router]);
+
+    return <Calendar onChange={onChange} value={value}/>;
 };
 
 export default EventCalendar;
