ee61e8f9815577676a2e1cb609fd45d8fc4b1a85
Bujar Roka <rokabujar@gmail.com>
2025-01-02 00:41:56 +0100

    updated the big calendar component to fetch data from database (for teacher and student)

--- schoolmanager/src/app/(dashboard)/teacher/page.tsx
+++ schoolmanager/src/app/(dashboard)/teacher/page.tsx
@@ -1,16 +1,18 @@
-"use client"
 
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalendar";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import { auth } from "@clerk/nextjs/server";
 
-const TeacherPage = () => {
+const TeacherPage = async () => {
+
+  const {userId} = await auth();
   return(
     <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
       {/* LEFT */}
       <div className="w-full xl:2/3">
       <div className="h-full bg-white p4 rounded-md">
         <h1 className="text-xl font-semibold">Schedule</h1>
-        <BigCalendar />
+        <BigCalendarContainer type="teacherId" id={userId!}/>
       </div>
       </div>
       {/* RIGHT */}
