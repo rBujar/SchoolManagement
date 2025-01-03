3e81b8af002f08268b8627d2696258cc603eee68
Bujar Roka <rokabujar@gmail.com>
2025-01-03 17:23:18 +0100

    added CRUD for class form (class list page)

--- schoolmanager/src/lib/actions.ts
+++ schoolmanager/src/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { SubjectSchema } from "./formValidationSchemas";
+import { ClassSchema, SubjectSchema } from "./formValidationSchemas";
 import prisma from "./prisma";
 
 type CurrentState = { success: boolean; error: boolean }
@@ -67,6 +67,65 @@ export const deleteSubject = async (
         });
 
         // revalidatePath("/list/subjects");
+
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err);
+        return { success: false, error: true };
+    }
+};
+// CLASS
+export const createClass = async (
+    currentState: CurrentState,
+    data: ClassSchema
+) => {
+    try {
+        await prisma.class.create({
+            data
+        });
+
+        // revalidatePath("/list/class");
+
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err);
+        return { success: false, error: true };
+    }
+};
+export const updateClass = async (
+    currentState: CurrentState,
+    data: ClassSchema
+) => {
+    try {
+        await prisma.class.update({
+            where:{
+                id:data.id
+            },
+            data
+        });
+
+        // revalidatePath("/list/class");
+
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err);
+        return { success: false, error: true };
+    }
+};
+export const deleteClass = async (
+    currentState: CurrentState,
+    data: FormData
+) => {
+
+    const id = data.get("id") as string
+    try {
+        await prisma.class.delete({
+            where:{
+                id:parseInt(id),
+            },
+        });
+
+        // revalidatePath("/list/class");
 
         return { success: true, error: false };
     } catch (err) {
