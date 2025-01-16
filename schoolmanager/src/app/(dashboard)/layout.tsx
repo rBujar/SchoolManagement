935778eaa87cf120a6eae3e8b89b44fe7a6e4136
Bujar Roka <rokabujar@gmail.com>
2025-01-16 20:26:33 +0100

    made the navbar username dynamic

--- schoolmanager/src/app/(dashboard)/layout.tsx
+++ schoolmanager/src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <Image src="/Ubtlogo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold"> SchoolManager</span>
         </Link>
         <Menu />
